92d7d6a3cccb914c48fa567fa75a469d93ab7d0c
Jessica Bommer <xininu@gmail.com>
2021-05-30 22:05:10 +1000

    delete unnecessary file

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import { App } from './App';
-// this test is failing, have to update
-test('renders select your ads and checkout link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/select your ads and checkout/i);
-  expect(linkElement).toBeInTheDocument();
-});
